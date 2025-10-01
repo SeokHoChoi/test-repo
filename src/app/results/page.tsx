601f31a9025d74fbd410db57bda15f791f2afbd8
SeokHoChoi <seokho9473@gmail.com>
2025-10-01 23:45:06 +0900

    feat: NBTI 설명 카드 > 텍스트 변경사항 반영

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -249,9 +249,11 @@ export default function ResultsPage() {
           subtitle="(Nutritional Body & Type Index)"
           description={
             <>
-              반려견의 건강 상태를 32가지 유형으로 나누고<br />
-              어떻게 하면 영양학적으로 더 건강하게 지낼 수<br />
-              있을지 알려주는 지표에요.
+              반려견의 가장 기본적인 정보를 <br />
+              32가지 유형으로 분류하는 시스템입니다.<br />
+              캐릭터는 각 유형을 이해하기 쉽게 일반화하여<br />
+              표현한 것이고, 건강 관리 팁은 우리 아이에게<br />
+              실제로 도움이 되는 맞춤 정보를 제공합니다.
             </>
           }
           buttons={[
