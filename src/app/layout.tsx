2b387666a4169c59230696a4414a088c6ec2823f
SeokHoChoi <seokho9473@gmail.com>
2025-10-02 13:10:12 +0900

    기본 링크 공유 시 2:1 비율 이미지 사용하도록 루트 레이아웃 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -102,7 +102,14 @@ export const metadata: Metadata = {
   openGraph: {
     title: "우리 아이의 NBTI는? | Jelly Univ",
     description: "🐶 우리 아이 건강 MBTI 테스트\n너의 갱얼쥐 NBTI가 뭐야? 🐾",
-    images: ['/img/jellyu-logo.png'],
+    images: [
+      {
+        url: '/img/kakao-share/kakao-test-share-800x400.png',
+        width: 800,
+        height: 400,
+        alt: 'NBTI 테스트 이미지',
+      },
+    ],
     siteName: '젤리대학교',
     type: 'website',
   },
@@ -110,6 +117,7 @@ export const metadata: Metadata = {
     card: 'summary_large_image',
     title: "우리 아이의 NBTI는? | Jelly Univ",
     description: "🐶 우리 아이 건강 MBTI 테스트\n너의 갱얼쥐 NBTI가 뭐야? 🐾",
+    images: ['/img/kakao-share/kakao-test-share-800x400.png'],
   },
 };
 
