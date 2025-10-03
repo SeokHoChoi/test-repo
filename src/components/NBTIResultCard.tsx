d10cfec12717b201cdc27ed7b4b418748ae45ba8
SeokHoChoi <seokho9473@gmail.com>
2025-10-03 23:46:01 +0900

    feat: 결과 카드 > 강아지 이미지 크기 조정

--- src/components/NBTIResultCard.tsx
+++ src/components/NBTIResultCard.tsx
@@ -26,21 +26,21 @@ export function NBTIResultCard({
         <p className="text-[#FFFFFF] font-medium text-[15px] mt-2">@젤리대학교</p>
       </div>
 
-      <div className="flex justify-center mb-9">
+      <div className="flex justify-center mb-11">
         {/* 고정 크기 컨테이너로 래핑하여 왜곡 방지 */}
-        <div className="relative z-10" style={{ width: 128, height: 128 }}>
+        <div className="relative z-10" style={{ width: 188, height: 188 }}>
           <img
             src={dogImage}
             alt={`${dogName} 강아지`}
-            width={128}
-            height={128}
+            width={188}
+            height={188}
             className="max-w-full max-h-full w-auto h-auto object-contain block"
             style={{ display: 'block' }}
           />
         </div>
       </div>
 
-      <div className="bg-white px-[20px] py-[35px] relative -mt-16 z-0 rounded-[30px]">
+      <div className="bg-white px-[20px] py-[35px] relative -mt-20 z-0 rounded-[30px]">
         {children}
       </div>
     </div>
