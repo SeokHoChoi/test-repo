c5c2b67f1db3d7d9c70798b9671dea5027b6e738
SeokHoChoi <seokho9473@gmail.com>
2025-10-03 23:54:31 +0900

    feat: 결과 카드 > 줄바꿈 간격 수정

--- src/app/results/share/page.tsx
+++ src/app/results/share/page.tsx
@@ -488,7 +488,7 @@ export default function SharePage() {
                 </p>
               </div>
 
-              <div className="text-center text-[#000000] font-normal text-[13px] leading-relaxed px-[50px] mb-[15px]">
+              <div className="text-center text-[#000000] font-normal text-[13px] leading-[18px] px-[50px] mb-[15px]">
                 {Array.isArray(result.nbti.description)
                   ? result.nbti.description.map((desc, index) => (
                     <p key={index} className="break-keep mb-2 last:mb-0">{desc}</p>
